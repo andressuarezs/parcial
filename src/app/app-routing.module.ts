7f65cc38621f290d1f67da2325ff34e9627ef8c2
cristian navaja <Cans121596@gmail.com>
2019-09-27 19:54:01 -0500

    Primercommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'grupoa',
+    children:[
+      {
+        path:'',
+        loadChildren:'./grupoa/grupoa.module#GrupoaModule'
+      }
+    ]
+  
+  },
+  {
+    path:'grupob',
+    children:[
+      {
+        path:'',
+        loadChildren:'./grupob/grupob.module#GrupobModule'
+      }
+    ]
+  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
